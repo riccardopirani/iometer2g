24b6bed811fae677a73c1157f6fd2ce2301c44f4
riccardopirani <riccardo@viewsoftware.it>
2023-09-18 12:07:07 +0200

    Get value from new api
    Get value from new api

--- HuaweiAPICore/Managers/IOMeterAPIClient.cs
+++ HuaweiAPICore/Managers/IOMeterAPIClient.cs
@@ -1,9 +1,9 @@
 ﻿using EILib.Dao.Managers;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
 using System.Net;
-using System.Reflection;
 using static EILib.Managers.RestClientManager;
 
 namespace HuaweiAPICore.Managers {
@@ -16,17 +16,25 @@ namespace HuaweiAPICore.Managers {
             string requestMethod = RestClientMethodEnum.GET;
             string url = baseURL + "/apps/iomtsgdata/v1/obisdescriptor";
             HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-
+            String responseString;
             request.Method = requestMethod;
             request.ContentType = "application/json";
 
             using (HttpWebResponse response = request.GetResponse() as HttpWebResponse) {
                 using (Stream responseStream = response.GetResponseStream()) {
                     StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
-                    string responseString = reader.ReadToEnd();
-                    Console.WriteLine(responseString);
+                    responseString = reader.ReadToEnd().ToString();
                 }
             }
+            if (responseString.Length > 0) {
+                dynamic json = JsonConvert.DeserializeObject(responseString);
+                foreach( var item in json) {
+                    Console.WriteLine(item);
+                }
+               
+            }
+
+
             return true;
         }
     }
